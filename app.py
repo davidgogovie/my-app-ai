1284092bffa2c5aba6a0e0a8fb86a0679a08f50a
davidgogovie <david.gogovie@yahoo.com>
2025-04-14 01:35:41 +0000

    Update app.py
    A new diffusion model code copied from my local computer

--- app.py
+++ app.py
@@ -1,3 +1,73 @@
+# import streamlit as st
+# st.write("My first Generative AI App")
+
+# import needed libraries
+import diffusers
+import torch
 import streamlit as st
 
-st.write("My first Generative AI App")
+# Set the device and `dtype` for GPUs
+if torch.cuda.is_available():
+    device = "cuda"
+    dtype = torch.float16
+else:
+    device = "cpu"
+    dtype = torch.float32
+    
+# The dictionary mapping style names to style strings
+style_dict = {
+    "none": "",
+    "anime": "cartoon, animated, Studio Ghibli style, cute, Japanese animation",
+    # A photograph on film suggests an artistic approach
+    "photo": "photograph, film, 35 mm camera",
+    "video game": "rendered in unreal engine, hyper-realistic, volumetric lighting, --ar 9:16 --hd --q 2",
+    "watercolor": "painting, watercolors, pastel, composition",
+}
+
+# Load Stable Diffusion (load_model function)
+def load_model():
+    MODEL_NAME = "CompVis/stable-diffusion-v1-4"
+    # pipeline = diffusers.StableDiffusionPipeline.from_pretrained(
+    pipeline = diffusers.AutoPipelineForText2Image.from_pretrained(
+        MODEL_NAME,
+        torch_dtype=dtype
+    )
+    # pipeline.load_lora_weights(
+    #     LORA_WEIGHTS, # Directory containing weights file
+    #     weight_name="pytorch_lora_weights.safetensors",
+    # )
+    pipeline.to(device)
+    return pipeline
+
+# The generate_images function
+def generate_images(prompt, pipeline, n, guidance=7.5, steps=50, style="none"):
+    return pipeline([prompt + ' ' + style_dict[style]] * n, guidance_scale=guidance, num_inference_steps=steps).images
+
+# The main function
+def main():
+    st.title("Stable Diffusion GUI")
+
+    num_images = st.sidebar.number_input("Number of Images", min_value=1, max_value=10)
+    prompt = st.sidebar.text_area("Text-to-Image Prompt")
+
+    guidance_help = "Lower values follow the prompt less strictly. Higher values risk distored images."
+    guidance = st.sidebar.slider("Guidance", 2.0, 15.0, 7.5, help=guidance_help)
+
+    steps_help = "More steps produces better images but takes longer."
+    steps = st.sidebar.slider("Steps", 10, 150, 50, help=steps_help)
+
+    style = st.sidebar.selectbox("Style", options=style_dict.keys())
+
+    generate = st.sidebar.button("Generate Images")
+    if generate:
+        with st.spinner("Generating images..."):
+            pipeline = load_model()
+            images = generate_images(
+                prompt, pipeline, num_images, guidance, steps, style
+            )
+            for im in images:
+                st.image(im)
+
+
+if __name__ == "__main__":
+    main()
